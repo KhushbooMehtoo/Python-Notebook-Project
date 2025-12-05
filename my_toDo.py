a39e280a93898d328147c7e581cbfd978880c64e
KhushbooMehto <khushboo200022@gmail.com>
2025-12-05 21:44:24 +0530

    basic-structure

---
+++ my_toDo.py
@@ -0,0 +1,31 @@
+# my to do....
+
+class Todo:
+
+    def __init__(self):
+        self.id = ""
+        self.note=""
+
+    def start_with(self):
+        print("Start with.\n 1.Create\n 2.Read \n 3.Update \n 4.Delete")
+
+    def get_details(self):
+        self.id = input("Enter the id: ")
+        self.note = input("Enter notes here")
+
+    def display_profile(self):
+        print(f"\nUser Notes:")
+        print(f"Id: {self.id}")
+        print(f"Note: {self.note}")
+
+        
+    def start(self):
+        
+        select =int(input(" Enter frome (1-4): "))
+        
+user1 = Todo()
+
+user1.start_with()
+user1.start()
+user1.get_details()
+user1.display_profile()
