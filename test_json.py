87a7a8d9770d8c32095839280aa93a7add025fef
KhushbooMehto <khushboo200022@gmail.com>
2025-12-13 23:49:36 +0530

    json to python testCase

--- test_json.py
+++ test_json.py
@@ -1,13 +1,28 @@
 
 import json
+#JSON String
+emp='{"id":"01","emp_name":"Khushboo","department":"Development"}'
+print("This is JSON String",type(emp))
+
+#convert into python 
+print("Convert From Json to Python")
+
+#convert string itno dictionary
+my_emp= json.loads(emp)
+print("convert into python",type(my_emp))
+print(my_emp)
+
+
+
+# ******************************........
 
 # JSON string
-d = {'id': '09', 'name': 'khushbu', 'department': 'development'}
-print("This is Python", type(d))
+# d = {'id': '09', 'name': 'khushbu', 'department': 'development'}
+# print("This is Python", type(d))
 
-print("\nNow Convert from Python to JSON")
+# print("\nNow Convert from Python to JSON")
 
-# Convert Python dict to JSON
-obj = json.dumps(d, indent=4)
-print("Converted to JSON", type(obj))
-print(obj)
+# # Convert Python dict to JSON
+# obj = json.dumps(d, indent=4)
+# print("Converted to JSON", type(obj))
+# print(obj)
